bc4245f806ade0fee0f35431078a35be40079ec7
Яков Хватов <YakovX95@mail.ru>
2024-11-30 19:15:21 +0300

    update css, added login page without logic update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Room from './pages/RoomPage';
-// import Login from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/login" element={< LoginPage />} />
                 <Route path="/" element={< Room />} />
             </Routes>
         </BrowserRouter>
