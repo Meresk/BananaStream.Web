b64d994ffd575d4a9ef5629cb936c80f711e1a88
Яков Хватов <YakovX95@mail.ru>
2024-12-03 18:36:05 +0300

    auth for teacher

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Room from './pages/RoomPage';
 import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StudentPage from './pages/StudentPage';
+import TeacherPage from './pages/TeacherPage';
 
 const App: React.FC = () => {
     return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
                 <Route path="/login" element={< LoginPage />} />
                 <Route path="/" element={< Room />} />
                 <Route path="/student" element={< StudentPage />} />
+                <Route path="/teacher" element={< TeacherPage />} />
             </Routes>
         </BrowserRouter>
     );
