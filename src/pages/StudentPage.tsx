9ed23d640a269f29078b6960e4a0324fd1b9a17e
Яков Хватов <YakovX95@mail.ru>
2024-12-05 02:27:56 +0300

    update student, teacher and main pages. Room logic added. MVP commit

--- src/pages/StudentPage.tsx
+++ src/pages/StudentPage.tsx
@@ -1,35 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { Typography, Card, CardContent, Grid, Box } from '@mui/material';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { checkAuth } from "../services/authService";
 import ClipLoader from "react-spinners/ClipLoader";
+import { useNavigate } from 'react-router-dom';
 import { Room } from "../types/Room.ts";
 
 const StudentPage: React.FC = () => {
     const navigate = useNavigate();
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true); // Индикатор загрузки
     const [rooms, setRooms] = useState<Room[]>([]);
 
     useEffect(() => {
-        const validateAuth = async () => {
-            const isAuthenticated = await checkAuth();
-            if (!isAuthenticated) {
-                navigate("/login");
-            } else {
-                setLoading(false);
-                fetchRooms(); // Загружаем комнаты после успешной аутентификации
-            }
-        };
+        // Первичная загрузка комнат
+        fetchRooms();
 
-        validateAuth();
-    }, [navigate]);
+        // Автоматическое обновление комнат каждые 10 секунд
+        const interval = setInterval(fetchRooms, 10000);
+
+        // Очистка интервала при размонтировании компонента
+        return () => clearInterval(interval);
+    }, []);
 
     const fetchRooms = async () => {
-        const response = await axios.get('http://127.0.0.1:3000/rooms');
-        setRooms(response.data);
+        try {
+            const response = await axios.get('http://127.0.0.1:3000/rooms');
+            setRooms(response.data);
+        } catch (error) {
+            console.error("Ошибка загрузки комнат:", error);
+        } finally {
+            setLoading(false); // Отключаем загрузку, даже если произошла ошибка
+        }
     };
 
+    const handleRoomClick = async (roomName: string) => {
+        const uniqueValue = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+
+        try {
+            const response = await axios.post('http://127.0.0.1:3000/getStudentToken', {
+                room: roomName,
+                identity: uniqueValue,
+            });
+
+            if (response.status === 200) {
+                const { token: roomToken } = response.data;
+                navigate('/room', { state: { roomToken, roomName } });
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                alert('Ошибка входа в комнату: ' + error.response?.data.error);
+            } else {
+                alert('Произошла неожиданная ошибка');
+            }
+        }
+    };
+
+    // Отображаем индикатор загрузки, пока комнаты не загружены
     if (loading) {
         return (
             <div
@@ -51,7 +76,7 @@ const StudentPage: React.FC = () => {
         <div
             style={{
                 display: "flex",
-                justifyContent: "center", // Центрируем список комнат
+                justifyContent: "center",
                 minHeight: "100vh",
                 width: "100%",
                 backgroundColor: "#121212",
@@ -59,21 +84,20 @@ const StudentPage: React.FC = () => {
                 boxSizing: 'border-box',
             }}
         >
-            {/* Блок для отображения существующих комнат */}
             <Box
                 sx={{
-                    flexGrow: 1, // Занимает всё доступное пространство
+                    flexGrow: 1,
                     backgroundColor: '#333',
-                    padding: '20px', // Внутренние отступы внутри блока
-                    margin: '20px', // Внешние отступы от краёв экрана
+                    padding: '20px',
+                    margin: '20px',
                     borderRadius: '8px',
                     color: 'white',
                     boxSizing: 'border-box',
-                    overflowY: 'auto', // Прокрутка, если список слишком длинный
-                    display: 'flex', // Flexbox для выравнивания
+                    overflowY: 'auto',
+                    display: 'flex',
                     flexDirection: 'column',
-                    alignItems: 'center', // Центрирование по горизонтали
-                    justifyContent: rooms.length === 0 ? 'center' : 'flex-start', // Центрирование по вертикали, если комнат нет
+                    alignItems: 'center',
+                    justifyContent: rooms.length === 0 ? 'center' : 'flex-start',
                 }}
             >
                 {rooms.length > 0 ? (
@@ -83,11 +107,45 @@ const StudentPage: React.FC = () => {
                         </Typography>
                         <Grid container spacing={3}>
                             {rooms.map((room, index) => (
-                                <Grid item xs={12} sm={6} md={4} key={index}>
-                                    <Card style={{ backgroundColor: '#444', color: 'white' }}>
-                                        <CardContent>
-                                            <Typography variant="h6">{room.name}</Typography>
-                                            <Typography variant="body2">
+                                <Grid item xs={12} sm={6} md={3} key={index}>
+                                    <Card
+                                        style={{
+                                            backgroundColor: '#FDFFAB',
+                                            color: 'white',
+                                            cursor: 'pointer',
+                                            borderRadius: '12px', // Скругленные углы
+                                            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)', // Тень
+                                            transition: 'transform 0.3s, box-shadow 0.3s', // Анимация при наведении
+                                        }}
+                                        onClick={() => handleRoomClick(room.name)}
+                                        onMouseEnter={(e) => {
+                                            (e.currentTarget as HTMLElement).style.transform = 'scale(1.05)';
+                                            (e.currentTarget as HTMLElement).style.boxShadow = '0px 8px 20px rgba(0, 0, 0, 0.5)';
+                                        }}
+                                        onMouseLeave={(e) => {
+                                            (e.currentTarget as HTMLElement).style.transform = 'scale(1)';
+                                            (e.currentTarget as HTMLElement).style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.3)';
+                                        }}
+                                    >
+                                        <CardContent style={{ padding: '20px', textAlign: 'center' }}>
+                                            <Typography
+                                                variant="h6"
+                                                style={{
+                                                    fontSize: '18px',
+                                                    fontWeight: 'bold',
+                                                    marginBottom: '10px',
+                                                    color: "black"
+                                                }}
+                                            >
+                                                {room.name}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                style={{
+                                                    color: '#606060', // Немного светлее для текста
+                                                    fontSize: '14px',
+                                                }}
+                                            >
                                                 {room.num_participants} котиков в комнате
                                             </Typography>
                                         </CardContent>
@@ -95,6 +153,7 @@ const StudentPage: React.FC = () => {
                                 </Grid>
                             ))}
                         </Grid>
+
                     </>
                 ) : (
                     <Box
@@ -103,16 +162,16 @@ const StudentPage: React.FC = () => {
                             flexDirection: 'column',
                             alignItems: 'center',
                             justifyContent: 'center',
-                            gap: '20px', // Расстояние между картинкой и текстом
+                            gap: '20px',
                         }}
                     >
                         <img
-                            src="../../public/zerorooms.png" // Замените на ссылку на вашу картинку
+                            src="/zerorooms.png"
                             alt="No rooms available"
                             style={{
                                 maxWidth: '150px',
                                 maxHeight: '150px',
-                                opacity: 0.8, // Лёгкая прозрачность
+                                opacity: 0.8,
                             }}
                         />
                         <Typography variant="h6" style={{ color: 'gray', textAlign: 'center' }}>
